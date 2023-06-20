755addfbfc834a816d5c607615ea81328c92a1f5
H01001000 <heiheiho000@gmail.com>
2023-06-20 15:56:41 -0700

    json

--- pages/src/app/api/hello/route.ts
+++ pages/src/app/api/hello/route.ts
@@ -2,10 +2,12 @@
 // Next.js Edge API Route Handlers: https://nextjs.org/docs/app/building-your-application/routing/router-handlers#edge-and-nodejs-runtimes
 
 import type { NextRequest } from 'next/server'
+import type { KVNamespace } from '@cloudflare/workers-types'
 
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
-  const data = await context.env.KV_STATUS_PAGE.get("monitors_data_v1_1");
-  return new Response(JSON.stringify(JSON.parse(data), undefined, '  '))
+  const { KV_STATUS_PAGE } = (process.env as unknown as { KV_STATUS_PAGE: KVNamespace });
+  const data = await KV_STATUS_PAGE.get("monitors_data_v1_1", { type: 'json' });
+  return new Response(JSON.stringify(data, undefined, '  '))
 }
