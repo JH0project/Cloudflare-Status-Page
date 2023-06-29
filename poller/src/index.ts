f6f57fc8cc65cb57118be2522003bd95929a93d5
H01001000 <heiheiho000@gmail.com>
2023-06-29 15:39:57 -0700

    fix(poller): false not a funcction

--- poller/src/index.ts
+++ poller/src/index.ts
@@ -9,25 +9,25 @@ import type { addEventListener as AddEventListener } from '@cloudflare/workers-t
  *    than the default 404.html page.
  */
 //@ts-ignore
-const DEBUG = false
+const DEBUG = false;
 
-  // addEventListener('fetch', (event) => {
-  //   try {
-  //     event.respondWith(
-  //       handleEvent(event, require.context('./pages/', true, /\.js$/), DEBUG),
-  //     )
-  //   } catch (e) {
-  //     if (DEBUG) {
-  //       return event.respondWith(
-  //         new Response(e.message || e.toString(), {
-  //           status: 500,
-  //         }),
-  //       )
-  //     }
-  //     event.respondWith(new Response('Internal Error', { status: 500 }))
-  //   }
-  // })
+// addEventListener('fetch', (event) => {
+//   try {
+//     event.respondWith(
+//       handleEvent(event, require.context('./pages/', true, /\.js$/), DEBUG),
+//     )
+//   } catch (e) {
+//     if (DEBUG) {
+//       return event.respondWith(
+//         new Response(e.message || e.toString(), {
+//           status: 500,
+//         }),
+//       )
+//     }
+//     event.respondWith(new Response('Internal Error', { status: 500 }))
+//   }
+// })
 
-  (addEventListener as typeof AddEventListener)('scheduled', (event) => {
-    event.waitUntil(processCronTrigger(event))
-  })
+(addEventListener as typeof AddEventListener)('scheduled', (event) => {
+  event.waitUntil(processCronTrigger(event))
+})
