828e87bbb34eb3a340402f9ea580e66a58739eca
H01001000 <heiheiho000@gmail.com>
2023-06-29 12:21:44 -0700

    chore(site): add google analytics

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Roboto } from 'next/font/google'
 import DefaultRootLayout from './RootLayout'
+import Script from 'next/script.js'
 
 const inter = Roboto({ weight: ['300', '400', '500', '700'], display: 'swap', subsets: ['cyrillic', 'cyrillic-ext', 'greek', 'greek-ext', 'latin', 'latin-ext', 'vietnamese'] })
 
@@ -16,6 +17,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className} style={{ minHeight: '100vh', color: '#FFF', backgroundColor: "#fff" }}>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-21C3KNWYDN" strategy='afterInteractive' />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-21C3KNWYDN');
+          `}
+        </Script>
         <DefaultRootLayout>
           {children}
         </DefaultRootLayout>
