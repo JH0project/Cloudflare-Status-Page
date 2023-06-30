8c374eaee0c83594355caba0e0f48bf6da6df8c0
H01001000 <heiheiho000@gmail.com>
2023-06-29 17:19:51 -0700

    feat(site): add second ago

--- site/src/app/Monitors.tsx
+++ site/src/app/Monitors.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { KvMonitors } from 'cf-status-page-types';
 import UptimeGraph from './UptimeGraph';
 import ResponseGraph from './ResponseGraph';
@@ -13,11 +13,12 @@ const Link = (args: Parameters<typeof MuiLink>[0]) => <MuiLink component={NextLi
 
 export default function Monitors({ kvMonitors }: { kvMonitors: KvMonitors }) {
   const router = useRouter()
+  const [now, setNow] = useState(Date.now())
 
   useEffect(() => {
     const interval = setInterval(() => {
-      router.refresh()
-    }, 2 * 60 * 60 * 1000)
+      setNow(Date.now())
+    }, 1000)
     return () => clearInterval(interval)
   }, [router])
 
@@ -26,12 +27,12 @@ export default function Monitors({ kvMonitors }: { kvMonitors: KvMonitors }) {
       <Typography variant='h3' component='h1' style={{ margin: '2.5vh 1vw' }}>
         {config.settings.title}
       </Typography >
-      <Paper elevation={5} style={{ padding: '2.5vh 2vw', margin: '0 0 5vh 0', backgroundColor: kvMonitors.allOperational ? '#2ecc71' : '' }}>
-        <Typography variant='h3' component='h1' style={{ color: '#fff', textAlign: 'left' }}>
+      <Paper elevation={5} style={{ padding: '2.5vh 2vw', margin: '0 0 5vh 0', backgroundColor: kvMonitors.allOperational ? '#2ecc71' : '', display: "flex", alignItems: 'center' }}>
+        <Typography variant='h3' component='h1' style={{ color: '#fff', textAlign: 'left', flex: "4 1 0" }}>
           {kvMonitors.allOperational ? 'All Systems Operational' : 'Outage'}
         </Typography >
-        <Typography variant='body1' component='p' style={{ color: '#fff', textAlign: 'right' }}>
-          {kvMonitors.lastCheck}
+        <Typography variant='body1' component='p' style={{ color: '#fff', textAlign: 'right', flex: "1 1 0" }}>
+          {`${Math.round((now - kvMonitors.lastCheck) / 1000)} Seconds ago`}
         </Typography >
       </Paper>
       <Paper elevation={5} style={{ padding: '5vh 0', margin: '5vh 0' }}>
