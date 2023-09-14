2aeac16c7dd3e70328865052aa9ff7081aa72d16
Jonathan Ho <heiheiho000@gmail.com>
2023-09-15 00:19:57 +0800

    Update page.tsx

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,4 +1,6 @@
-import 'server-only'
+"use client"
+
+//import 'server-only'
 import type { KVNamespace } from '@cloudflare/workers-types'
 import type { KvMonitors } from 'cf-status-page-types'
 import { notFound } from 'next/navigation.js';
@@ -9,10 +11,10 @@ export const dynamic = 'force-dynamic'
 export const runtime = 'edge'
 
 const getKvMonitors = async (): Promise<KvMonitors | null> => {
-  if (!process.env.KV_STATUS_PAGE)
-    return fetch(`${config.settings.url}/api/status`).then((res) => res.status === 200 ? res.json() : null);
-  const { KV_STATUS_PAGE } = (process.env as unknown as { KV_STATUS_PAGE: KVNamespace });
-  return KV_STATUS_PAGE.get("monitors_data_v1_1", { type: 'json' });
+  //if (!process.env.KV_STATUS_PAGE)
+  return fetch(`${config.settings.url}/api/status`).then((res) => res.status === 200 ? res.json() : null);
+  //const { KV_STATUS_PAGE } = (process.env as unknown as { KV_STATUS_PAGE: KVNamespace });
+  //return KV_STATUS_PAGE.get("monitors_data_v1_1", { type: 'json' });
 }
 
 export default async function Home() {
