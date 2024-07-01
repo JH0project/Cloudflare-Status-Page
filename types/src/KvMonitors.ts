d464db28e99be8532cda8f0099e7498534466e68
H01001000 <heiheiho000@gmail.com>
2024-06-30 18:08:04 -0700

    keep types for site

--- types/src/KvMonitors.ts
+++ types/src/KvMonitors.ts
@@ -1,3 +1,40 @@
+export interface Check {
+  incidents: number[],
+  summery: {
+    [index: string]: {
+      n: number
+      ms: number
+      a: number
+    }
+  },
+  res: {
+    t: number
+    loc: string
+    ms: number
+  }[]
+}
+
+export interface KvMonitor {
+  operational: boolean
+  incidents: {
+    start: number
+    status: number
+    statusText: string
+    end?: number
+  }[],
+  checks: {
+    [index: string]: Check
+  }
+}
+
+export interface KvMonitors {
+  lastCheck: number
+  allOperational: boolean
+  monitors: {
+    [index: string]: KvMonitor
+  }
+}
+
 export interface Checks {
   // incidents: {
   //   [index: string]: {
