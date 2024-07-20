ac880eab0098cfe9550d59dd0cdb776f0eba3ef4
H01001000 <heiheiho000@gmail.com>
2024-07-20 03:02:12 -0700

    refactor: use v2 format

--- site/src/app/OverallResponseGraph.tsx
+++ site/src/app/OverallResponseGraph.tsx
@@ -1,24 +1,31 @@
 "use client"
 
+import { Checks } from 'cf-status-page-types';
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-export default function ResponseGraph({ data, day = 90 }: { data: { day: string, summery: { [index: string]: { a: number } } }[], day?: number }) {
+export default function ResponseGraph({ monitorName, checks, day = 90 }: {
+  monitorName: string, checks: {
+    [index: string]: Checks
+  }, day?: number
+}) {
   const now = new Date().setUTCHours(0, 0, 0, 0) + 86400000;
   const locations: string[] = []
   const processedData = []
-  const dataInMs = data.map((checkDay) => ({ ...checkDay, day: new Date(checkDay.day).getTime() }))
+  const dataInMs = Object.keys(checks).map((checkDay) => ({ ...checks[checkDay], day: new Date(checkDay).getTime() }))
   for (let dayBefore = day - 1; dayBefore > -1; dayBefore--) {
     const item = dataInMs.find((checkDay) => checkDay.day === now - 86400000 * dayBefore)
-    if (item !== undefined) {
-      Object.keys(item.summery).forEach((location) => {
-        if (!locations.includes(location)) locations.push(location)
-      });
-      processedData.push(item)
+    if (item === undefined) {
+      processedData.push({ day: now - 86400000 * dayBefore })
       continue
     }
-    processedData.push({ day: now - 86400000 * dayBefore })
+
+    Object.keys(item.summery).forEach((location) => {
+      if (!locations.includes(location)) locations.push(location)
+    });
+    processedData.push(item)
   }
+
 
   return (
     <ResponsiveContainer width="100%" height="100%">
@@ -33,7 +40,7 @@ export default function ResponseGraph({ data, day = 90 }: { data: { day: string,
         <Tooltip labelStyle={{ color: '#000' }} labelFormatter={(label, _payload) => new Date(label).toISOString().slice(0, 10)} />
         {/* <Line type="monotone" dot={false} dataKey="ms" stroke="#82ca9d" /> */}
         {locations.map((location) =>
-          <Line type="monotone" connectNulls dot={false} dataKey={(d) => d.summery?.[location]?.a ?? null} stroke="#82ca9d" name={location} unit=' ms' key={location} />
+          <Line type="monotone" connectNulls dot={false} dataKey={(d) => d.summery?.[location]?.[monitorName].a ?? null} stroke="#82ca9d" name={location} unit=' ms' key={location} />
         )}
       </LineChart>
     </ResponsiveContainer>
--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -2,32 +2,112 @@
 
 //import 'server-only'
 import type { KVNamespace } from '@cloudflare/workers-types'
-import type { MonthSummery } from 'cf-status-page-types'
-import { notFound } from 'next/navigation.js';
+import type { MonitorMonth, MonthSummery } from 'cf-status-page-types'
+import { notFound } from 'next/navigation';
 import Monitors from './Monitors';
 import config from '../../../config.json'
 import { useEffect, useState } from 'react';
+import { Container, Typography, Paper, Divider, Box } from '@mui/material';
+import Link from 'next/link';
+import AllStatus from './AllStatus';
+import OverallResponseGraph from './OverallResponseGraph';
+import UptimeGraph from './UptimeGraph';
 
 export const dynamic = 'force-dynamic'
 export const runtime = 'edge'
 
-const getKvMonitors = async (): Promise<any | null> => {
+const getKvMonitors = async (key: string): Promise<MonitorMonth> => {
   //if (!process.env.KV_STATUS_PAGE)
-  return fetch(`${config.settings.url}/api/status`).then((res) => res.status === 200 ? res.text() : null);
+  return fetch(`${config.settings.url}/api/kv/${key}`).then((res) => {
+    if (res.ok) {
+      return res.json()
+    }
+    throw new Error('Failed to fetch')
+  });
   //const { KV_STATUS_PAGE } = (process.env as unknown as { KV_STATUS_PAGE: KVNamespace });
   //return KV_STATUS_PAGE.get("monitors_data_v1_1", { type: 'json' });
 }
 
+const getYearMonth = (date: Date) => {
+  return date.toISOString().split('T')[0].slice(0, 7)
+}
+
 export default function Home() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState<MonitorMonth>({
+    checks: {},
+    lastCheck: 0,
+    operational: {},
+  })
 
   useEffect(() => {
-    getKvMonitors().then((res) => { setData(JSON.parse(res)) })
+    getKvMonitors(getYearMonth(new Date())).then((res) => {
+      setData(oldData => ({
+        checks: { ...oldData.checks, ...res.checks },
+        lastCheck: res.lastCheck > oldData.lastCheck ? res.lastCheck : oldData.lastCheck,
+        operational: res.lastCheck > oldData.lastCheck ? res.operational : oldData.operational,
+      }))
+    }).catch(() => { });
+    const lastMonth = new Date()
+    lastMonth.setMonth(lastMonth.getMonth() - 1)
+
+    getKvMonitors(getYearMonth(lastMonth)).then((res) => {
+      setData(oldData => ({
+        checks: { ...oldData.checks, ...res.checks },
+        lastCheck: res.lastCheck > oldData.lastCheck ? res.lastCheck : oldData.lastCheck,
+        operational: res.lastCheck > oldData.lastCheck ? res.operational : oldData.operational,
+      }))
+    }).catch(() => { });
+    const last2Month = new Date()
+    last2Month.setMonth(last2Month.getMonth() - 2)
+
+    getKvMonitors(getYearMonth(last2Month)).then((res) => {
+      setData(oldData => ({
+        checks: { ...oldData.checks, ...res.checks },
+        lastCheck: res.lastCheck > oldData.lastCheck ? res.lastCheck : oldData.lastCheck,
+        operational: res.lastCheck > oldData.lastCheck ? res.operational : oldData.operational,
+      }))
+    }).catch(() => { });
   }, [])
 
   return (
-    <>
-      {data && <Monitors kvMonitors={data} />}
-    </>
+
+    <Container maxWidth='md' style={{ padding: '0 0 5vh 0' }}>
+      <Typography variant='h3' component='h1' style={{ margin: '2.5vh 1vw' }}>
+        {config.settings.title}
+      </Typography >
+      <AllStatus data={data} />
+      <Paper elevation={5} style={{ padding: '5vh 0', margin: '5vh 0' }}>
+        <Container>
+          {Object.keys(data.operational).map((monitorName, i) => {
+            console.log(data)
+            return (<Box key={i}>
+              {i !== 0 && <Divider style={{ margin: '2.5vh 0' }} />}
+              <Typography variant='h6' component='h2' style={{ color: data.operational[monitorName] ? '#2ecc71' : '' }}>
+                <Link style={{ color: 'inherit' }} underline='hover' href={config.monitors[i].url}>
+                  {config.monitors[i].name}
+                </Link>
+                <span style={{ float: 'right', color: data.operational[monitorName] ? '#3BA55C' : '' }}>{data.operational[monitorName] ? 'Operational' : 'Outage'}</span>
+              </Typography >
+              {/* <UptimeGraph kvMonitor={kvMonitor} monitorName={monitorName} key={monitorName} /> */}
+              <div style={{ height: '20vh', width: '100%' }}>
+                <OverallResponseGraph checks={data.checks} monitorName={monitorName} />
+              </div>
+            </Box>)
+          })}
+        </Container>
+      </Paper >
+      <div>
+        <Typography>
+          <Link href='https://github.com/JH0project/Cloudflare-Status-Page'>
+            Cloudflare Status Page
+          </Link>
+          {' by '}
+          <Link href='https://github.com/H01001000'>
+            H01001000
+          </Link>
+        </Typography>
+        <Typography>{'Powered by '}<Link href='https://www.cloudflare.com/'>Cloudflare</Link>{' and '}<Link href='https://nextjs.org/'>Next.js</Link></Typography>
+      </div>
+    </Container >
   )
 }
